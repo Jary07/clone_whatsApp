2df904ab8bfaba232cc99ea9ed8cd4e84d736a0a
Ja Ry <143274687+Jary07@users.noreply.github.com>
2023-11-17 13:20:49 +0000

    restore

---
+++ 3rd/3rd.js
@@ -0,0 +1,7 @@
+let message=document.getElementsByClassName("message");
+// let MessageSheet=getElementByClass
+
+
+message.addEventListener("click",function(){
+    location.href= "../2nd/message-sheet.html";
+})
