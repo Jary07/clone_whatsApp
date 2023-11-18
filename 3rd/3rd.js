91537617358b1965b577c508758ecf909812bd6d
Ja Ry <143274687+Jary07@users.noreply.github.com>
2023-11-18 10:18:14 +0000

    again

--- 3rd/3rd.js
+++ 3rd/3rd.js
@@ -1,10 +1,8 @@
 let message=document.getElementById("name1");
 let change="3rdpage.html";
 
-Array.from(message);
-
 message.addEventListener("click",function(){
     window.location.href= "message-sheet.html";
 
-    message.style.backgroundColor="red";
+    // message.style.backgroundColor="red";
 })
