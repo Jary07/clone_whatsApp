1a2e52077fe525e0d6fc5d1014abb697220d99c3
Ja Ry <143274687+Jary07@users.noreply.github.com>
2023-11-18 13:28:30 +0000

    added feature

--- 3rd/3rd.js
+++ 3rd/3rd.js
@@ -2,7 +2,7 @@ let message=document.getElementById("name1");
 let change="3rdpage.html";
 
 message.addEventListener("click",function(){
-    window.location.href= "message-sheet.html";
+    window.location.href= "ethan/message-sheet.html";
 
     // message.style.backgroundColor="red";
 })
